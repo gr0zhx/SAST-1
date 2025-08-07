e3f3bbaa4e11e60f456ecc8c4425c196a74f7d0e
Agry Zharfa <139311001+gr0zhx@users.noreply.github.com>
2025-08-07 16:06:43 +0700

    Update Currency.java

--- src/currencyConverter/Currency.java
+++ src/currencyConverter/Currency.java
@@ -1,18 +1,18 @@
-package currency.converter;  // diperbaiki sesuai regex naming convention
+package currencyconverter;
 
 import java.util.List;
-import java.util.Map;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.HashMap;
 
-public class Converter {
-
+public class Currency {
     private String name;
     private String shortName;
     private Map<String, Double> exchangeValues;
 
-    public Converter(String name) {
+    public Currency(String name, String shortName) {
         this.name = name;
+        this.shortName = shortName;
         this.exchangeValues = new HashMap<>();
         defaultValues();
     }
@@ -26,15 +26,25 @@ public class Converter {
                 break;
             case "EUR":
                 exchangeValues.put("USD", 1.18);
-                exchangeValues.put("JPY", 129.53);
+                exchangeValues.put("JPY", 129.0);
                 break;
-            // Default case ditambahkan untuk menghindari bug maintainability
+            case "JPY":
+                exchangeValues.put("USD", 0.0091);
+                exchangeValues.put("EUR", 0.0078);
+                break;
             default:
+                // Default case to ensure maintainability
                 exchangeValues.put("USD", 1.0);
-                exchangeValues.put("EUR", 1.0);
-                exchangeValues.put("JPY", 1.0);
                 break;
         }
+    }
+
+    public static List<Currency> init() {
+        List<Currency> currencies = new ArrayList<>();
+        currencies.add(new Currency("USD", "US Dollar"));
+        currencies.add(new Currency("EUR", "Euro"));
+        currencies.add(new Currency("JPY", "Japanese Yen"));
+        return currencies;
     }
 
     public void setShortName(String shortName) {
@@ -45,29 +55,11 @@ public class Converter {
         return exchangeValues;
     }
 
-    public static List<Currency> init() {
-        List<Currency> currencies = new ArrayList<>();
-        currencies.add(new Currency("Dollar", "USD"));
-        currencies.add(new Currency("Euro", "EUR"));
-        currencies.add(new Currency("Yen", "JPY"));
-        return currencies;
-    }
-}
-
-class Currency {
-    private String name;
-    private String shortName;
-
-    public Currency(String name, String shortName) {
-        this.name = name;
-        this.shortName = shortName;
+    public String getShortName() {
+        return shortName;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getShortName() {
-        return shortName;
     }
 }
