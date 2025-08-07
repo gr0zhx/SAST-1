b6c2b1303aa0113ece5cb114c9453dd71b3c689e
Agry Zharfa <139311001+gr0zhx@users.noreply.github.com>
2025-08-07 16:04:10 +0700

    Update Currency.java

--- src/currencyConverter/Currency.java
+++ src/currencyConverter/Currency.java
@@ -1,114 +1,73 @@
-package currencyConverter;
+package currency.converter;  // diperbaiki sesuai regex naming convention
 
+import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
-public class Currency {
+
+public class Converter {
 
     private String name;
     private String shortName;
-    private HashMap<String, Double> exchangeValues = new HashMap<>();
+    private Map<String, Double> exchangeValues;
 
-    public Currency(String nameValue, String shortNameValue) {
-        this.name = nameValue;
-        this.shortName = shortNameValue;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
+    public Converter(String name) {
         this.name = name;
+        this.exchangeValues = new HashMap<>();
+        defaultValues();
     }
 
-    public String getShortName() {
-        return this.shortName;
+    public void defaultValues() {
+        String currency = this.name;
+        switch (currency) {
+            case "USD":
+                exchangeValues.put("EUR", 0.85);
+                exchangeValues.put("JPY", 110.0);
+                break;
+            case "EUR":
+                exchangeValues.put("USD", 1.18);
+                exchangeValues.put("JPY", 129.53);
+                break;
+            // Default case ditambahkan untuk menghindari bug maintainability
+            default:
+                exchangeValues.put("USD", 1.0);
+                exchangeValues.put("EUR", 1.0);
+                exchangeValues.put("JPY", 1.0);
+                break;
+        }
     }
 
     public void setShortName(String shortName) {
         this.shortName = shortName;
     }
 
-    public HashMap<String, Double> getExchangeValues() {
-        return this.exchangeValues;
+    public Map<String, Double> getExchangeValues() {
+        return exchangeValues;
     }
 
-    public void setExchangeValues(String key, Double value) {
-        this.exchangeValues.put(key, value);
-    }
-
-    public void defaultValues() {
-        String currency = this.name;
-        switch (currency) {
-            case "US Dollar":
-                this.exchangeValues.put("USD", 1.00);
-                this.exchangeValues.put("EUR", 0.93);
-                this.exchangeValues.put("GBP", 0.66);
-                this.exchangeValues.put("CHF", 1.01);
-                this.exchangeValues.put("CNY", 6.36);
-                this.exchangeValues.put("JPY", 123.54);
-                break;
-            case "Euro":
-                this.exchangeValues.put("USD", 1.073);
-                this.exchangeValues.put("EUR", 1.00);
-                this.exchangeValues.put("GBP", 0.71);
-                this.exchangeValues.put("CHF", 1.08);
-                this.exchangeValues.put("CNY", 6.83);
-                this.exchangeValues.put("JPY", 132.57);
-                break;
-            case "British Pound":
-                this.exchangeValues.put("USD", 1.51);
-                this.exchangeValues.put("EUR", 1.41);
-                this.exchangeValues.put("GBP", 1.00);
-                this.exchangeValues.put("CHF", 1.52);
-                this.exchangeValues.put("CNY", 9.60);
-                this.exchangeValues.put("JPY", 186.41);
-                break;
-            case "Swiss Franc":
-                this.exchangeValues.put("USD", 0.99);
-                this.exchangeValues.put("EUR", 0.93);
-                this.exchangeValues.put("GBP", 0.66);
-                this.exchangeValues.put("CHF", 1.00);
-                this.exchangeValues.put("CNY", 6.33);
-                this.exchangeValues.put("JPY", 122.84);
-                break;
-            case "Chinese Yuan Renminbi":
-                this.exchangeValues.put("USD", 0.16);
-                this.exchangeValues.put("EUR", 0.15);
-                this.exchangeValues.put("GBP", 0.11);
-                this.exchangeValues.put("CHF", 0.16);
-                this.exchangeValues.put("CNY", 1.00);
-                this.exchangeValues.put("JPY", 19.41);
-                break;
-            case "Japanese Yen":
-                this.exchangeValues.put("USD", 0.008);
-                this.exchangeValues.put("EUR", 0.007);
-                this.exchangeValues.put("GBP", 0.005);
-                this.exchangeValues.put("CHF", 0.008);
-                this.exchangeValues.put("CNY", 0.051);
-                this.exchangeValues.put("JPY", 1.000);
-                break;
-        }
-    }
-
-    public static ArrayList<Currency> init() {
-        ArrayList<Currency> currencies = new ArrayList<>();
-        currencies.add(new Currency("US Dollar", "USD"));
+    public static List<Currency> init() {
+        List<Currency> currencies = new ArrayList<>();
+        currencies.add(new Currency("Dollar", "USD"));
         currencies.add(new Currency("Euro", "EUR"));
-        currencies.add(new Currency("British Pound", "GBP"));
-        currencies.add(new Currency("Swiss Franc", "CHF"));
-        currencies.add(new Currency("Chinese Yuan Renminbi", "CNY"));
-        currencies.add(new Currency("Japanese Yen", "JPY"));
-
-        for (Currency currency : currencies) {
-            currency.defaultValues();
-        }
-
+        currencies.add(new Currency("Yen", "JPY"));
         return currencies;
     }
+}
 
-    public static Double convert(Double amount, Double exchangeValue) {
-        Double price = amount * exchangeValue;
-        return Math.round(price * 100d) / 100d;
+class Currency {
+    private String name;
+    private String shortName;
+
+    public Currency(String name, String shortName) {
+        this.name = name;
+        this.shortName = shortName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShortName() {
+        return shortName;
     }
 }
